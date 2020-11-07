53da755d5ebf6d0e64850cb8cadddd6bb4e9df0c
Tom Czarniecki <tomcz@users.noreply.github.com>
2020-11-07 20:30:45 +1000

    might want to know why shutdown panics

--- cmd/example/exit.go
+++ cmd/example/exit.go
@@ -39,7 +39,7 @@ func withCancel(shutdown func()) (context.Context, func()) {
 	return ctx, func() {
 		defer func() {
 			if r := recover(); r != nil {
-				log.Printf("panic during shutdown: %v", r)
+				log.Printf("shutdown panic: %v, stack %s\n", r, string(debug.Stack()))
 			}
 		}()
 		cancel()
